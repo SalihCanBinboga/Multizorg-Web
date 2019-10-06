4b6e257dcfb08e20c99f9160ccc717181900e97f
eldemtr <eldemtr@gmail.com>
2019-10-06 23:46:56 +0300

    no message

--- inc/header.php
+++ inc/header.php
@@ -21,6 +21,7 @@ echo '<!DOCTYPE html>
     //<meta name="viewport" content="width=device-width, initial-scale=1">
     '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="favicon.ico">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/0.8.2/css/flag-icon.min.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">'.
     //<link href="css/bootstrap.min.css" rel="stylesheet">
     '
